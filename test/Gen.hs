1bde1a62ced830dfaa6d101bb01350b7fa5eb040
Frederick Pringle <freddyjepringle@gmail.com>
2022-12-27 16:30:00 +0100

    generators for bookmarks; some basic tests

---
+++ test/Gen.hs
@@ -0,0 +1,46 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Gen where
+
+import HoYo
+
+import Test.QuickCheck
+
+import System.FilePath
+
+import Data.Time
+
+letter :: Gen Char
+letter = elements (['a'..'z'] <> ['A'..'Z'] <> ['0'..'9'])
+
+word :: Gen String
+word = listOf1 letter
+
+genFilePath :: Gen FilePath
+genFilePath = joinPath <$> listOf word
+
+genZonedTime :: Gen ZonedTime
+genZonedTime = ZonedTime <$> genLocalTime <*> genTimeZone
+  where
+    genLocalTime = LocalTime <$> (ModifiedJulianDay <$> arbitrary)
+                             <*> (TimeOfDay <$> chooseInt (0, 23)
+                                            <*> chooseInt (0, 59)
+                                            <*> arbitrary)
+
+    genTimeZone = TimeZone <$> ((* 60) <$> chooseInt (-12, 12))
+                           <*> arbitrary
+                           <*> vectorOf 3 letter
+
+genBookmark :: Gen Bookmark
+genBookmark = Bookmark <$> genFilePath
+                       <*> chooseInt (1, 1000)
+                       <*> genZonedTime
+                       <*> liftArbitrary word
+
+genBookmarks :: Gen Bookmarks
+genBookmarks = Bookmarks <$> listOf genBookmark
+
+genBookmarkSearchTerm :: Gen BookmarkSearchTerm
+genBookmarkSearchTerm = oneof [
+  SearchIndex <$> chooseInt (1, 1000)
+  , SearchName <$> word
+  ]
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,37 @@
 module Main (main) where
 
+import HoYo
+
+import Gen
+
+import Test.QuickCheck
+
+testBookmarkSearch' :: (BookmarkSearchTerm, Bookmarks) -> Property
+testBookmarkSearch' (search, bms) =
+  let
+    (sat, notsat) = searchBookmarks search bms
+    satGood = conjoin $ map (matchBm search) sat
+    notsatGood = conjoin $ map (not . matchBm search) notsat
+  in satGood .&&. notsatGood
+
+  where
+    matchBm (SearchName s) = (== Just s) . _bookmarkName
+    matchBm (SearchIndex i) = (== i) . _bookmarkIndex
+
+testBookmarkSearchSuccess' :: (Bookmark, Bookmarks) -> Property
+testBookmarkSearchSuccess' (one, Bookmarks rest) = testBookmarkSearch' (search, Bookmarks (one:rest))
+  where
+    search = case _bookmarkName one of
+                Just name   -> SearchName name
+                Nothing     -> SearchIndex (_bookmarkIndex one)
+
+testBookmarkSearch :: Property
+testBookmarkSearch = forAll ((,) <$> genBookmarkSearchTerm <*> genBookmarks) testBookmarkSearch'
+
+testBookmarkSearchSuccess :: Property
+testBookmarkSearchSuccess = forAll ((,) <$> genBookmark <*> genBookmarks) testBookmarkSearchSuccess'
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  quickCheck testBookmarkSearch
+  quickCheck testBookmarkSearchSuccess
